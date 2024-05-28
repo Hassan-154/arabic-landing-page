0754f3700214b1b96328c482f5837bbe8a773cce
hassan <hassanbajwa154@gmail.com>
2024-05-29 02:36:13 +0500

    update

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -21,10 +21,10 @@ function About() {
 
   return (
     <main className="relative">
-      <Image className="h-[100vh]" width='full' height='full' src={backGround} alt="Background image of كال القيمة company" />
+      <Image className={` ${language === 'en' ? 'h-[115vh]' : 'h-[100vh]'}`}  width='full' height='full' src={backGround} alt="Background image of كال القيمة company" />
       <div className="absolute top-0 left-0 right-0 flex justify-center pt-8">
       <article className={`flex flex-col gap-4 max-w-[900px] mx-auto text-[23px] sm:text-[30px] font-medium px-3 ${language === 'en' ? 'text-left' : 'text-right'}`}>
-          <h1 className="text-brown text-[28px] sm:text-[38px] py-2">عن شركة كال القيمة</h1>
+          <h1 className="text-brown text-[28px] sm:text-[38px] py-2">{data.title}</h1>
           {data.services && (
             <ul className={`flex flex-col gap-6 ${language === 'en' ? 'gap-3' : 'gap-6'}`}>
               {data.services.map((item, id) => (
