0a959b89aa41906a635b2dfdddec21855ab06180
hassan <hassanbajwa154@gmail.com>
2024-05-29 22:53:52 +0500

    update

--- src/app/components/Form.js
+++ src/app/components/Form.js
@@ -8,7 +8,7 @@ function Form() {
     return (
         <div className='bg-[#f9f7f4]'>
             <div className='relative overflow-x-hidden max-w-[1000px] mx-auto '>
-                <div className='h-[70vh] lg:h-[100vh]'></div>
+                <div className='h-[70vh] sm:h-[80vh] md:h-[100vh]'></div>
                 <Image className='absolute right-3 top-[200px]' src={rightCircle} alt='Right circle' width='270' height='270' />
                 <Image className='absolute left-0 top-0' src={leftCircle} alt='Left circle' width='370' height='270' />
                 <Image className='absolute inset-0 top-[140px] flex items-center justify-center px-3' src={formImage} alt='Form image' width='full' height='full' />
